7d67c61e576f4a03237b61f31567000200884dd3
ryaanwells <ryan.wells@skyscanner.net>
2015-05-05 11:40:32 +0100

    Added UTF-8 support for client.

--- luigi_bigquery/client.py
+++ luigi_bigquery/client.py
@@ -27,15 +27,18 @@ class ResultProxy(object):
         return rows
 
     def to_csv(self, path_or_file):
+        def _write_row(f, values):
+            line = u",".join([v if type(v) is unicode else unicode(str(v), encoding='UTF-8') for v in values]) + u"\n"
+            f.write(line.encode('UTF-8'))
+
         def _to_csv(f):
-            f.write(",".join(self._columns()))
-            f.write("\n")
+            _write_row(f, self._columns())
             for row in self._rows():
-                f.write(",".join([str(c) if c else '' for c in row]) + "\n")
+                _write_row(f, row)
 
         if type(path_or_file) in [str, unicode]:
-            with file(path_or_file, 'w') as f:
-                return _to_csv(f)
+            with open(path_or_file, 'w', encoding='UTF-8') as csv_file:
+                return _to_csv(csv_file)
         else:
             return _to_csv(path_or_file)
 
