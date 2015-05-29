68c6f54bc26688ae6c5faa80201087c48979403d
Kazuyuki Honda <hakobera@gmail.com>
2015-05-29 23:55:25 +0900

    Support ResultTarget

--- luigi_bigquery/task.py
+++ luigi_bigquery/task.py
@@ -1,7 +1,10 @@
 from luigi_bigquery.config import get_config
 from luigi_bigquery.client import ResultProxy
+from luigi_bigquery.job import Job
+from luigi_bigquery.targets.result import ResultTarget
 
 import luigi
+import jinja2
 import time
 
 import logging
@@ -15,10 +18,17 @@ class QueryTimeout(Exception):
 class Query(luigi.Task):
     config = get_config()
     debug = False
-    timeout = None
+    timeout = 3600
+    source = None
+    variables = {}
 
     def query(self):
         return NotImplemented()
+
+    def load_query(self, source):
+        env = jinja2.Environment(loader=jinja2.PackageLoader(self.__module__, '.'))
+        template = env.get_template(source)
+        return template.render(task=self, **self.variables)
 
     def run_query(self, query):
         result = self.output()
@@ -26,7 +36,7 @@ class Query(luigi.Task):
         job_id, _results = client.query(self.query())
         logger.info("%s: bigquery.job.id: %s", self, job_id)
 
-        complete, row_count = client.check_job(job_id)
+        complete, result_size = client.check_job(job_id)
         try:
             if self.timeout:
                 timeout = time.time() + self.timeout
@@ -37,19 +47,23 @@ class Query(luigi.Task):
                 if timeout and time.time() > timeout:
                     raise QueryTimeout('{0} timed out'.format(self))
                 time.sleep(5)
-                complete, row_count = client.check_job(job_id)
+                complete, result_size = client.check_job(job_id)
         except:
             raise
 
-        logger.info("%s: bigquery.job.result: job_id=%s row_count=%d", self, job_id, row_count)
+        logger.info("%s: bigquery.job.result: job_id=%s result_size=%d", self, job_id, result_size)
 
-        schema = client.get_query_schema(job_id)
-        results = client.get_query_rows(job_id)
-        return ResultProxy(job_id, schema, row_count, results)
+        return ResultProxy(Job(client, job_id))
 
     def run(self):
-        query = self.query()
+        if hasattr(self, 'query_file'):
+            self.source = self.query_file
+        query = self.load_query(self.source) if self.source else self.query()
         result = self.run_query(query)
+        target = self.output()
+
+        if target and isinstance(target, ResultTarget):
+            target.save_result_state(result)
 
         if self.debug:
             import pandas as pd
